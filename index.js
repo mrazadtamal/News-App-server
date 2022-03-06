eab0d9eabec7dac33bcdb56962c0699a251e4938
mrazadtamal <mrazadt@gmail.com>
2022-03-06 22:11:49 +0600

    data

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const notes = require("./data/notes");
 const app = express();
 
 const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("ok");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const singleNote = notes.find((n) => n._id === req.params.id);
+  res.send(singleNote);
 });
 
 app.listen(port, () => {
